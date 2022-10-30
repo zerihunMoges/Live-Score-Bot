ce3e4e8f0ceee0add89e4923a5d9078c4b193617
Zerihun Moges <91532718+zerihunMoges@users.noreply.github.com>
2022-10-30 11:34:57 +0300

    add files

---
+++ bot/bot.js
@@ -0,0 +1,23 @@
+const token = '5320053436:AAFN1ylnc9sAvjXzIEVyHZyQ2607Cgma7KU'
+
+const { Telegraf } = require('telegraf')
+const link = 'https://venerable-youtiao-6ebbe5.netlify.app'
+const bot = new Telegraf(token)
+bot.start((ctx) => {
+  console.log(ctx.message)
+  ctx.reply('Click the button below to use Live Score Bot', {
+    reply_markup: {
+      inline_keyboard: [[{ text: 'Pl Live', web_app: { url: link } }]]
+    }
+  })
+})
+
+bot.hears('send', (ctx) =>
+  ctx.reply('Click the button below to use Live Score Bot', {
+    reply_markup: {
+      inline_keyboard: [[{ text: 'Live Score', web_app: { url: link } }]]
+    }
+  })
+)
+
+bot.launch()
